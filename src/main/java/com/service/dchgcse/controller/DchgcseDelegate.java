e1ad0849662f5fe2e80d52ee3bb60e03a76575cc
paas <paas@cse-codegen-backend-3747876825-lv3d4>
2018-07-30 01:54:00 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/dchgcse/controller/DchgcseDelegate.java
@@ -0,0 +1,14 @@
+package com.service.dchgcse.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class DchgcseDelegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
